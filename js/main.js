1b46c902eed5a6e8b7b2fe97631fbf9124c6da13
HOOOO98 <loading2us@gmail.com>
2023-08-07 17:40:10 +0900

    Docs: modify main.js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,37 @@
-// Header intro menu box
-const introEL = document.querySelector('header .mainmenu__intro');
-const intro__basketEl = document.querySelector('header .mainmenu__intro__basket')
+// 변수 선언
+const introel = document.querySelector('header .mainmenu__intro');
+const intro__basketel = document.querySelector('header .mainmenu__intro__basket')
+const languageel = document.querySelector('header .mainmenu__language');
+const language__basketel = languageel.querySelector('.mainmenu__language__basket');
+const familysiteel = document.querySelector('footer .attachment__rightitem');
+const familysite__basketel = familysiteel.querySelector('.familysite_basket');
 
-introEL.addEventListener('mouseover', function () {
-    if (intro__basketEl.classList.contains('show')) {
-        intro__basketEl.classList.remove('show');
-    } else {
-        intro__basketEl.classList.add('show')
-    }
+//common function
+function toggleShowClick(el, basketel) {
+    el.addEventListener('click', function (event) {
+      event.stopPropagation();
+      basketel.classList.toggle('show');  
+    })
+
+    window.addEventListener('click', function() {
+        basketel.classList.remove('show');
+    })
+}
+
+// Header intro menu box
+introel.addEventListener('mouseover', function () {
+    intro__basketel.classList.toggle('show');
 })
 
 window.addEventListener('mouseout', function (event) {
     const from = event.relatedTarget;
-    if (!introEL.contains(from) && !intro__basketEl.contains(from)) {
-        intro__basketEl.classList.remove('show');
+    if (!introel.contains(from) && !intro__basketel.contains(from)) {
+        intro__basketel.classList.remove('show');
     }
 });
 
 // Header language box
-const languageEL = document.querySelector('header .mainmenu__language');
-const language__basketEL = languageEL.querySelector('.mainmenu__language__basket');
-
-languageEL.addEventListener('click', function (event) {
-    event.stopPropagation();
-    if (language__basketEL.classList.contains('show')) {
-        language__basketEL.classList.remove('show');
-    } else {
-        language__basketEL.classList.add('show');
-    }
-    
-})
-
-window.addEventListener('click', function () {
-    language__basketEL.classList.remove('show')
-})
+toggleShowClick(languageel, language__basketel);
 
 // Footer family sites box
-const familysiteEL = document.querySelector('footer .attachment__rightitem');
-const familysite__basketEL = familysiteEL.querySelector('.familysite_basket');
-
-familysiteEL.addEventListener('click', function (event) {
-    event.stopPropagation();
-    if (familysite__basketEL.classList.contains('show')) {
-        familysite__basketEL.classList.remove('show')
-    }   else {
-        familysite__basketEL.classList.add('show')
-    }
-})
-
-window.addEventListener('click', function () {
-    familysite__basketEL.classList.remove('show')
-})
+toggleShowClick(familysiteel, familysite__basketel);
